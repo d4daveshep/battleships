4a65af3ccb87cac6e714697057cd94d2aae7ac32
d4daveshep <david@daveshep.net.nz>
2025-08-10 18:02:30 +1200

    syntax correction

--- tests/component/test_game_flow.py
+++ tests/component/test_game_flow.py
@@ -1,12 +1,12 @@
-def test_ft_game_play():
-    """Functional test: Test the game play"""
+def test_game_play():
+    """Component test: Test the game model through the flow of some game rounds"""
 
     # Alice and Bob decide to start a game
     # Alice and Bob place thier ships on their game boards
     # When they are both done placing ships the game is ready to start
     # Alice and Bob both have 5 shots available to fire
     # They both aim (place) their shots and fire them at each others ships
-    # This completes the round and thei game reports the results of the round
+    # This completes the round and the game reports the results of the round
     # The game informs them of any ships they've hit with their shots in this round
     # The game informs them on any hits on their ships and if any have been sunk
     # The game informs then of the number of shots available for the next round

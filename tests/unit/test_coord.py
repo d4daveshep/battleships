a2f00395d6bf49044c6a03d7ee73495096643990
d4daveshep <david@daveshep.net.nz>
2025-11-10 07:35:41 +1300

    Added validation testing for Coord strings based on regex for 10x10 grid.

--- tests/unit/test_coord.py
+++ tests/unit/test_coord.py
@@ -1,6 +1,11 @@
 import pytest
 from game.model import Coord
 
+validation_test_data: list[tuple[str, bool]] = [
+    ("A1", True),
+    ("1A", False),
+]
+
 
 class TestCoordCreation:
     def test_create_coord_from_string(self):
@@ -9,5 +14,10 @@ class TestCoordCreation:
         assert coord.col == 1
         assert coord.row_index == 1
 
-    def test_coord_validation(self):
-        assert False
+    @pytest.mark.parametrize("coord, expected", validation_test_data)
+    def test_coord_validation(self, coord: str, expected: bool):
+        if expected:
+            Coord(coord)
+        else:
+            with pytest.raises(ValueError):
+                Coord(coord)

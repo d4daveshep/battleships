776bad972a8f9cf62736185bc0e78c4f89f39180
d4daveshep <david@daveshep.net.nz>
2025-11-22 09:10:15 +1300

    Changed drone-exec.sh to user root to be consistent across devices

--- drone-exec.sh
+++ drone-exec.sh
@@ -4,4 +4,4 @@
 docker build -f Dockerfile.ci -t battleships:latest .
 
 # run the CI pipeline
-docker run --rm --group-add 999 -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):$(pwd) -w $(pwd) drone/cli:latest exec
+docker run --rm --user root -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):$(pwd) -w $(pwd) drone/cli:latest exec
